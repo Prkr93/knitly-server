25cabce32afc9284e400a42068ac911449eeb7b3
Dillon Parker <prkr93@gmail.com>
2022-04-25 00:45:32 -0600

    Initial commit

---
+++ data.js
@@ -0,0 +1,25 @@
+const patterns = [
+  {
+    '1': ['knit', 'purl', 'knit', 'purl', 'knit'],
+    '2': ['purl', 'knit', 'purl', 'knit', 'purl'],
+    '3': ['knit', 'purl', 'knit', 'purl', 'knit'],
+    '4': ['purl', 'knit', 'purl', 'knit', 'purl'],
+    '5': ['knit', 'purl', 'knit', 'purl', 'knit']
+  },
+  {
+    '1': ['knit', 'knit', 'knit', 'knit', 'knit'],
+    '2': ['purl', 'purl', 'purl', 'purl', 'purl'],
+    '3': ['knit', 'knit', 'knit', 'knit', 'knit'],
+    '4': ['purl', 'purl', 'purl', 'purl', 'purl'],
+    '5': ['knit', 'knit', 'knit', 'knit', 'knit']
+  },
+  {
+    '1': ['knit', 'purl', 'knit', 'purl', 'knit'],
+    '2': ['knit', 'purl', 'knit', 'purl', 'knit'],
+    '3': ['knit', 'purl', 'knit', 'purl', 'knit'],
+    '4': ['knit', 'purl', 'knit', 'purl', 'knit'],
+    '5': ['knit', 'purl', 'knit', 'purl', 'knit']
+  }
+];
+
+module.exports = patterns;
---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const patterns = require('./data.js');
+
+app.use(cors());
+app.use(express.json());
+
+app.locals.title = 'Knitly Patterns';
+app.locals.patterns = [...patterns];
+
+app.set('port', 3001);
+app.listen(app.get('port'), () => {
+  console.log(`${app.locals.title} is running on http://localhost:${app.get('port')}.`);
+});
+
+app.get('/inspirations', (request, response) => {
+  let resPatterns = JSON.stringify(app.locals.patterns);
+  response.status(200).send(resPatterns);
+});
+
+app.post('/inspire', (request, response) => {
+  const pattern = request.body;
+  app.locals.patterns.push(pattern)
+  response.status(201);
+});
